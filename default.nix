41f7c66e8964e18a43fd09ee477189bd676cb990
Guilherme Gomes Haetinger <ghaetinger@gmail.com>
2022-08-20 19:03:51 -0300

    :poop: Audio testing using FrequencyAdjustment.jl

--- default.nix
+++ default.nix
@@ -2,26 +2,9 @@
 
 with pkgs;
 
-let
-
-mach-nix = import (builtins.fetchGit {                                                                                
-  url = "https://github.com/DavHau/mach-nix";                                                                         
-  ref = "refs/tags/3.5.0";                                                                                            
-}) {};                                                                                                                
-
-python_stuff = mach-nix.mkPython {                                                                                    
-  # rawpy
-  requirements = ''
-      opencv
-  '';                                                                                             
-}; 
-
-in
-
 mkShell {
-  buildInputs = [ python_stuff julia_17-bin ];
+  buildInputs = [ julia_17-bin ];
   shellHook = ''
-  export PYTHON=${python_stuff}/bin/python
-  export PYTHONPATH=${python_stuff}/lib/python3.9/site-packages:$PYTHONPATH
+    figlet -f slant "Welcome to my Dump!"
   '';
 }
